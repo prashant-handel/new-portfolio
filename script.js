1362d3a851e7078cd3057e899a0fdb76f3155bc5
prashant_handel <prashanthandel2501@gmail.com>
2022-12-16 11:54:48 +0530

    Hamburger Menu added

--- script.js
+++ script.js
@@ -1,70 +1,85 @@
-window.addEventListener('scroll', (e) => {
-    console.log(window.scrollY);
-  })
+// window.addEventListener("scroll", (e) => {
+//   console.log(window.scrollY);
+// });
 
-  let footerEl = document.querySelector("footer");
-  const headerLinks = document.querySelectorAll(".navLink");
+let footerEl = document.querySelector("footer");
+const headerLinks = document.querySelectorAll(".navLink");
 
-  const footerLinks = document.querySelectorAll(".footerLink");
+const footerLinks = document.querySelectorAll(".footerLink");
 
-  const contactMeBtnEl = document.querySelector(".contactMeBtn");
-  const contactMeEl = document.querySelector("#contactMe");
+const contactMeBtnEl = document.querySelector(".contactMeBtn");
+const contactMeEl = document.querySelector("#contactMe");
+
+headerLinks.forEach((link) => {
+  const linkIdEl = document.getElementById(link.getAttribute("data-link"));
+  link.addEventListener("click", () => {
+    window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 60);
+  });
+});
+
+footerLinks.forEach((link) => {
+  const linkIdEl = document.getElementById(link.getAttribute("data-link"));
+  link.addEventListener("click", () => {
+    window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 60);
+  });
+});
+
+
+contactMeBtnEl.addEventListener("click", () => {
+  window.scrollTo(0, contactMeEl.offsetTop - footerEl.offsetHeight + 60);
+});
+
+const scrollDownEl = document.getElementById("scrollDown");
+
+const linkIdEl = document.getElementById(
+  scrollDownEl.getAttribute("data-link")
+  );
   
-  headerLinks.forEach(link => {
-    const linkIdEl = document.getElementById(link.getAttribute("data-link"));
-    link.addEventListener("click", ()=>{
-      window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 60);
-      console.log(footerEl.offsetHeight);
-    })
+  scrollDownEl.addEventListener("click", () => {
+    window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 40);
   });
   
-  footerLinks.forEach(link => {
-    const linkIdEl = document.getElementById(link.getAttribute("data-link"));
-    link.addEventListener("click", ()=>{
-      window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 60);
-    })
-  });
-
-  contactMeBtnEl.addEventListener("click",()=>{
-    window.scrollTo(0, contactMeEl.offsetTop - footerEl.offsetHeight + 60)
-  })
-  
-  const scrollDownEl = document.getElementById("scrollDown");
-  
-      const linkIdEl = document.getElementById(scrollDownEl.getAttribute("data-link"));
-  
-        scrollDownEl.addEventListener("click", ()=>{
-          window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 40);
-        })
-    
-
   const upScrollEl = document.getElementById("upScroll");
+  
+upScrollEl.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
 
-  upScrollEl.addEventListener("click",()=>{
-    window.scrollTo(0,0);
-  })
- 
-  window.addEventListener("scroll", ()=>{
-    if(window.pageYOffset == 0){
-      upScrollEl.style.display = "none";
-    }
-    else{
-      upScrollEl.style.display = "block";
-    }
-  })
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset == 0) {
+    upScrollEl.style.display = "none";
+  } else {
+    upScrollEl.style.display = "block";
+  }
+});
 
-  let hamburgerNavEl = document.getElementById('hamburgerNav');
-  let hamburgerIconEl = document.getElementById('hamburgerIcon');
-  let closeIconEl = document.getElementById('closeIcon');
+let hamburgerNavEl = document.getElementById("hamburgerNav");
+let hamburgerIconEl = document.getElementById("hamburgerIcon");
+let closeIconEl = document.getElementById("closeIcon");
 
-  hamburgerIconEl.addEventListener("click",()=>{
-      hamburgerNavEl.style.display = "block";
-      hamburgerNavEl.style.width = "50vw";
-      closeIconEl.style.display = "block";
-    })
-    
-    closeIconEl.addEventListener("click",()=>{
+hamburgerIconEl.addEventListener("click", () => {
+  hamburgerNavEl.style.display = "block";
+  hamburgerNavEl.style.width = "50vw";
+  closeIconEl.style.display = "block";
+  hamburgerIconEl.style.display = "none";
+});
+
+closeIconEl.addEventListener("click", () => {
+  hamburgerNavEl.style.display = "none";
+  hamburgerNavEl.style.width = "0";
+  closeIconEl.style.display = "none";
+  hamburgerIconEl.style.display = "block";
+});
+
+const hamburgerLinks = document.querySelectorAll(".hamburgerLink");
+
+hamburgerLinks.forEach((link) => {
+  const linkIdEl = document.getElementById(link.getAttribute("data-link"));
+  link.addEventListener("click", () => {
+    window.scrollTo(0, linkIdEl.offsetTop - footerEl.offsetHeight + 60);
     hamburgerNavEl.style.display = "none";
     hamburgerNavEl.style.width = "0";
     closeIconEl.style.display = "none";
-  })
+    hamburgerIconEl.style.display = "block";
+  });
+});
